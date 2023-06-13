9b308de37e7ed7a30a8257ad71f0fe5af961b281
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-14 00:04:08 +0530

    [Ritvik]Add: Using Interface to make code flexible and reusable

---
+++ Interfaces/InterfaceConcept.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface ICustomer
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+
+        decimal productAmount { get; set; }
+
+        decimal CalculateDiscount();
+    }
+
+    public interface ICustomerGreeting : ICustomer
+    {
+        string GreetingMessage();
+    }
+
+    public abstract class Customer : ICustomer, ICustomerGreeting
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public decimal productAmount { get; set; }
+        public abstract decimal CalculateDiscount();
+
+        public abstract string GreetingMessage();
+    }
+
+    public class GoldCustomer : Customer
+    {
+        public override decimal CalculateDiscount()
+        {
+            return productAmount - 10;
+        }
+
+        public override string GreetingMessage()
+        {
+            return "Thank You ;-) ";
+        }
+    }
+
+    public class SilverCustomer : Customer
+    {
+        public override decimal CalculateDiscount()
+        {
+            return productAmount - 20;
+        }
+
+        public override string GreetingMessage()
+        {
+            return " Thank you. Visit again.";
+        }
+    }
+}
--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Interface concepts!!!");
+            Console.WriteLine("Welcome to Interface concepts!!!\n");
+
+            GoldCustomer obj = new GoldCustomer();
+            obj.Name = "Ramesh";
+            obj.Address = "Bangalore";
+            obj.productAmount = 100;
+
+
+            Console.WriteLine("Name = " + obj.Name + "\nAddress = " + obj.Address + "\nProductAmount = " + obj.productAmount +
+                                        "\nProductAmount after discount = " + obj.CalculateDiscount() + "\nGreetingMessage = " + obj.GreetingMessage());
+
+            SilverCustomer obj2 = new SilverCustomer();
+            obj2.Name = "Suresh";
+            obj2.Address = "Bangalore";
+            obj2.productAmount = 100;
+            Console.WriteLine("\nName = " + obj2.Name + "\nAddress = " + obj2.Address + "\nProductAmount = " + obj2.productAmount +
+                                        "\nProductAmount after discount = " + obj2.CalculateDiscount() + "\nGreetingMessage = " + obj2.GreetingMessage());
+
+            Console.Read();
         }
     }
 }
